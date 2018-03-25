03e86f7ed6172aa422b99891824bf9dd1325dbdc
akline101 <akline101@GMAIL.COM>
2018-03-25 15:03:14 +0800

    initial commit

--- functions.php
+++ functions.php
@@ -14,5 +14,20 @@ function my_change_featured_size( $image_sizes ) {
 }
 add_filter( 'et_theme_image_sizes', 'my_change_featured_size' );
 
+//add animate.css and wow.js files for aimation effects
+//* Enqueue Animate.CSS and WOW.js
+add_action( 'wp_enqueue_scripts', 'sk_enqueue_scripts' );
+function sk_enqueue_scripts() {
+    wp_enqueue_style( 'animate', get_stylesheet_directory_uri() . '/css/animate.min.css' );
+    wp_enqueue_script( 'wow', get_stylesheet_directory_uri() . '/js/wow.min.js', array(), '', true );
+}
+//* Enqueue script to activate WOW.js
+add_action( 'wp_enqueue_scripts', 'sk_wow_init_in_footer' );
+function sk_wow_init_in_footer() { add_action( 'print_footer_scripts', 'wow_init' );}
+//* Add JavaScript before
+function wow_init() { ?>
+    <script type="text/javascript">
+        new WOW().init(); </script>
+<?php }
 
 ?>
